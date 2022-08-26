1a342d16ed16eacc2515544188d8b328d95bfc6e
Alex Treviso <alextreviso12@gmail.com>
2022-08-26 00:13:35 -0300

    add sg in vpc-link

--- variables.tf
+++ variables.tf
@@ -3,3 +3,4 @@ variable "env" {}
 
 variable "alb_listener_arn" {}
 variable "aws_subnet_ids" {}
+variable "sg_alb_id" {}
--- vpc-link.tf
+++ vpc-link.tf
@@ -1,6 +1,7 @@
 resource "aws_apigatewayv2_vpc_link" "vpc-link" {
   name               = "${var.app_name}-vpc-link"
   subnet_ids         = var.aws_subnet_ids
+  security_group_ids = [var.sg_alb_id]
 
   tags = {
     Name         = "${var.app_name}-vpc-link"
